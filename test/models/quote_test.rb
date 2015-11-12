c92ff78f37bc5370fb97c4b51aaf1e9724e96172
vagrant <you@example.comwritercassiejeon@gmail.com>
2015-11-12 13:06:36 +0000

    Ready for commit

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class QuoteTest < ActiveSupport::TestCase
   
   test "unique_tag" do
-  	quote = FactoryGirl.create(:quote, :author => 'Cassie Jeon')
-	expected = 'CJ#' + quote.id.to_s
-	actual = quote.unique_tag
-	assert_equal expected, actual
+    quote = FactoryGirl.create(:quote, :author => 'Cassie Jeon')
+    expected = 'CJ#' + quote.id.to_s
+    actual = quote.unique_tag
+    assert_equal expected, actual
   end
 end
